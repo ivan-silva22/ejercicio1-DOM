2485a684b81cc5c902d095b9d3fb531b01a55a9d
Ivan Silva <silvaivan280@gmail.com>
2023-04-15 15:00:25 -0300

    Agregue dos funciones GenerarNumeroAleatorio y NumeroUsuario

---
+++ js/app.js
@@ -0,0 +1,33 @@
+let btnGenerarNumero = document.querySelector('#generarNumero');
+let formulario = document.querySelector('form');
+let numeroAleatorio;
+
+btnGenerarNumero.addEventListener('click',GenerarnumeroAleatorio);
+formulario.addEventListener('submit',numeroUsuario);
+
+function GenerarnumeroAleatorio(){
+    console.log('Generar numero aleatorio')
+    numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+    alert("¡Comencemos! elegi un número del 1 al 100. ¡Intenta adivinarlo!");
+    console.log(numeroAleatorio);
+    return numeroAleatorio;
+}
+
+function numeroUsuario(e){
+    e.preventDefault();
+    let numeroIngresado = parseInt(document.querySelector('input').value);
+    console.log(numeroIngresado);
+    if (numeroIngresado === numeroAleatorio) {
+        alert("¡Felicitaciones! Adivinaste el número.");
+      } else if (numeroIngresado < numeroAleatorio) {
+        alert("Lo siento, el número que ingresaste es menor al número mágico.");
+      } else {
+        alert("Lo siento, el número que ingresaste es mayor al número mágico.");
+      }
+
+}
+
+
+
+
+
